db9d011131cdbc1e88a4786b9afd8fe1606fe80a
user <user@localhost.localdomain>
2013-10-03 00:48:47 -0700

    Not really refactored, just easier to read.

--- summary.hs
+++ summary.hs
@@ -1,43 +1,57 @@
 module Main where
 
-import System.IO
-import System.Directory
 import System.Environment
 import Data.List
 import qualified Data.Map as Map
 
-sentenceScores :: [String] -> Int -> [Float]
-sentenceScores [] _ = []
-sentenceScores p sn = do
-    let s = (\ paragraph sentence -> last . fst $ splitAt (sentence+1) paragraph) p sn
-    map (sentencesIntersect s) $ p \\ [s]
+type Paragraph = [Sentence]
+type Sentence = String
+type Word = String
+type Score = Float
+type SentenceNumber = Int
+type ParagraphScores = Map.Map String Float
 
 
-paragraphScores :: [String] -> Map.Map String Float
-paragraphScores p = do
-    let scores = foldl' (\ scores sentence -> scores ++ [(sentenceScores p (head $ findIndices (==sentence) p))]) [] p
-    Map.fromList $ zip p $ map (\ scoreList -> foldl' (+) 0.0 scoreList) scores
+getUniqueWords :: Sentence -> [Word]
+getUniqueWords = nub . words
 
 
-sentencesIntersect :: String -> String -> Float
-sentencesIntersect sen1 sen2 = do
-    let s1          = nub $ words sen1
-        s2          = nub $ words sen2
-        commonWords = intersect s1 s2
-        averageLength = (fromIntegral ((length s1)+length s2)) / 2
+sentencesIntersect :: Sentence -> Sentence -> Score
+sentencesIntersect sentence1 sentence2 = do
+    let uniqueWords   = (getUniqueWords sentence1, getUniqueWords sentence2)
+        commonWords   = intersect (fst uniqueWords) (snd uniqueWords)
+        averageLength = (fromIntegral ((length $ fst uniqueWords) + (length $ snd uniqueWords))) / 2
     (fromIntegral (length commonWords)) / averageLength
+
+
+sentenceScores :: Paragraph -> SentenceNumber -> [Score]
+sentenceScores [] _ = []
+sentenceScores paragraph sentenceNumber = do
+    let getSentence = last . fst . splitAt (sentenceNumber + 1)
+        sentence = getSentence paragraph 
+    map (sentencesIntersect sentence) $ paragraph \\ [sentence]
+
+
+paragraphScores :: Paragraph -> ParagraphScores
+paragraphScores paragraph = do
+    let getSentence sentence = head . findIndices (==sentence) 
+        getSentenceScores = sentenceScores paragraph . flip getSentence paragraph
+        scoreMatrix = foldl' (\ scoreList sentence -> scoreList ++ [getSentenceScores sentence]) [] paragraph
+        sentenceScoreTotal = map sum scoreMatrix
+    Map.fromList $ zip paragraph sentenceScoreTotal 
+    
 
 
 -- | TODO : Does not currently generate a summary, only returns the
 -- | scoresmap for the first paragraph
-genSummary :: [Map.Map String Float] -> String
-genSummary [scoresMap] = undefined
+genSummary :: [ParagraphScores] -> String
+genSummary = undefined
 
-
+main :: IO ()
 main = do
     args <- getArgs
     contents <- readFile (args !! 0)
     -- | This is a god awful way of getting paragraphs/sentences from the
     -- | input txt, only used for testing
-    lines <- return $ lines contents
-    putStrLn $ show (paragraphScores lines)
+    inputLines <- return $ lines contents
+    putStrLn $ show (paragraphScores inputLines)
